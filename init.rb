1fd0926743b4963689607d5d029b425edb7c583a
Eugene Dubinin <eugend@commandprompt.com>
2024-01-05 12:52:26 +0200

    changes required redmine version makes plugin zeitwerk compatible

--- init.rb
+++ init.rb
@@ -5,17 +5,9 @@ Redmine::Plugin.register :redmine_assignable_watchers do
   author 'Eugene Dubinin <eugend@commandprompt.com>'
   author_url 'https://www.commandprompt.com'
   description 'Allows assigning the issues to any of its watchers'
-  version '0.2.1'
-  requires_redmine :version_or_higher => '3.4'
+  version '0.3.0'
+  requires_redmine :version_or_higher => '5.0'
   url 'https://github.com/commandprompt/redmine_assignable_watchers'
 end
 
-prepare_block = Proc.new do
-  Issue.send(:include, RedmineAssignableWatchers::IssuePatch)
-end
-
-if Rails.env.development?
-  ((Rails.version > "5")? ActiveSupport::Reloader : ActionDispatch::Callbacks).to_prepare { prepare_block.call }
-else
-  prepare_block.call
-end
+Issue.send(:include, RedmineAssignableWatchers::IssuePatch)
